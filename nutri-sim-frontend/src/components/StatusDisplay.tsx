a66b8018cff55a83b9267f5e28e081efdac4dde9
Christian Schwarz <schwarz-chr@web.de>
2022-11-28 08:59:58 +0100

    added body figure and highlighting animations

--- nutri-sim-frontend/src/components/StatusDisplay.tsx
+++ nutri-sim-frontend/src/components/StatusDisplay.tsx
@@ -1,10 +1,15 @@
-import status_figure from "../assets/status_figure.svg"
+import status_figure from "../assets/bladder/bladder.initial.webp"
 import "../styles/StatusDisplay.css"
 
 function StatusDisplay() {
 	return <div className="status-display">
 		<img src={status_figure} className="status-figure" alt="status figure" />
+		<button onClick={animate}>Show Bladder</button>
 	</div>
+}
+
+function animate() {
+	
 }
 
 export { StatusDisplay }
